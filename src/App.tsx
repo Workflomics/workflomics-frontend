8079a57141e2324e2af32f25626c7fa38d711d1b
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2023-01-27 15:02:40 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, {FC } from 'react';
+import './App.css';
+
+const App:FC =  () => {
+  
+  const platform_name: string = "Workflomics"
+
+  return (
+    <div className="App">
+      {platform_name}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomeBox.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+export function HomeBox (props: IAppProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
