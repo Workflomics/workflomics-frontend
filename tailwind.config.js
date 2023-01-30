c9a34a310f5cd6310df8d481c446acce228e2ca1
Peter Kok <kretep@users.noreply.github.com>
2023-01-30 16:23:28 +0100

    Add mobx, tailwind, daisy

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        "flomics-theme": {
+          primary: "#d70c0c",
+          secondary: "#f6d860",
+          accent: "#cd373a",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+          ".btn-primary": {
+            "color": "#fff"
+          }
+        },
+      },
+    ]
+  }
+}
