fc3c4a9e34ab3bc0da6fa312e00393fa7fe762bf
Peter Kok <kretep@users.noreply.github.com>
2023-01-30 16:24:35 +0100

    Some home screen styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,17 @@ const App:FC =  () => {
 
   return (
     <div className="App">
-      {platform_name}
-      <HomeBox title="Explore workflow" desc="Exploration of workflows" imgUrl=""/>
+      
+      <div className="text-5xl font-bold">
+        <h1>{platform_name}</h1>
+      </div>
+
+      <div className="flex space-x-7 p-11">
+        <HomeBox label="Explore workflow" descr="Exploration of workflows" imgUrl="" buttonText="Explore" isEnabled={true}/>
+        <HomeBox label="Compose & benchmark" descr="Automatically compose and benchmark workflows" imgUrl="" buttonText="Compose" isEnabled={true}/>
+        <HomeBox label="Benchmark workflows" descr="Evaluate the quality of your workflows" imgUrl="" buttonText="Benchmark" isEnabled={true}/>
+        <HomeBox label="My workflows" descr="Explore discovered and uploaded workflows" imgUrl="" buttonText="Workflows" isEnabled={true}/>
+      </div>
     </div>
   );
 }
--- src/components/HomeBox.tsx
+++ src/components/HomeBox.tsx
@@ -2,19 +2,30 @@ import * as React from 'react';
 
 interface Props {
     /** Title of the menu option. */
-    title: string;
+    label: string;
     /** Description of the menu option. */
-    desc : string;
+    descr : string;
+    /** Text on button */
+    buttonText: string;
     /** URL of the Thumbnail image of the menu option. */
     imgUrl: string;
     /** True if the menu is available only to registered users. */
-    loginRequired?: boolean;
+    isEnabled: boolean
 }
 
-export function HomeBox (props: Props) {
+export function HomeBox ({ label, descr, buttonText, imgUrl, isEnabled }: Props) {
   return (
-    <div>
-      <h1>{props.title}</h1>
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img src={imgUrl} alt={label} className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{label}</h2>
+        <p>{descr}</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">{buttonText}</button>
+        </div>
+      </div>
     </div>
   );
 }
