a183db99591eb8f596308e4acc87e6e1230f0bab
Rob Marissen <r.j.marissen@lumc.nl>
2024-02-16 16:11:15 +0100

    Remove superfluous items in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,28 +6,6 @@ interface Props {
 export function Footer({ }: Props) {
   return (
     <div>
-      <footer className="footer p-10 bg-base-200 text-base-content">
-        <div>
-          <span className="footer-title">Services</span>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </div>
-        <div>
-          <span className="footer-title">Company</span>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </div>
-        <div>
-          <span className="footer-title">Legal</span>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </div>
-      </footer>
       <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
         <div className="items-center grid-flow-col">
           <p>Developed by <a href="http://www.https://www.esciencecenter.nl/" >Netherlands eSceince Center</a> in collaboration with <a href="https://www.lumc.nl/" >LUMC (Leids Universitair Medisch Centrum)</a> and <a href="https://www.uni-potsdam.de/en/university-of-potsdam">University of Potsdam</a>.
