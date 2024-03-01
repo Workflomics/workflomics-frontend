d2f4d8faf61a06f939faf457fbcc1b69b1eb98f9
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2024-03-01 23:53:45 +0100

    Add the initial linting dependencies

--- src/stores/BenchmarkTypes.ts
+++ src/stores/BenchmarkTypes.ts
@@ -1,32 +1,31 @@
-
 export type BenchmarkRun = {
-  workflowName: string,
-  executor: string,
-  runID: string,
+  workflowName: string;
+  executor: string;
+  runID: string;
   inputs: {
     [input_id: string]: {
-      filename: string
-    }
-  },
-  benchmarks: WorkflowBenchmark[]
-}
+      filename: string;
+    };
+  };
+  benchmarks: WorkflowBenchmark[];
+};
 
 export type WorkflowBenchmark = {
-  title: string,
-  description: string,
-  unit: string,
-  aggregate_value: BenchmarkValue,
-  steps: BenchmarkValue[],
-}
+  title: string;
+  description: string;
+  unit: string;
+  aggregate_value: BenchmarkValue;
+  steps: BenchmarkValue[];
+};
 
 export type BenchmarkValue = {
-  label?: string,
-  value: string | number | boolean,
-  desirability: number,  // A number between -1 and 1 that will be used to calculate the color
-  tooltip?: string,
-}
+  label?: string;
+  value: string | number | boolean;
+  desirability: number; // A number between -1 and 1 that will be used to calculate the color
+  tooltip?: string;
+};
 
 export type DesigntimeBenchmarks = {
-  workflow_name: string,
-  benchmarks: WorkflowBenchmark[]
-}
+  workflow_name: string;
+  benchmarks: WorkflowBenchmark[];
+};
