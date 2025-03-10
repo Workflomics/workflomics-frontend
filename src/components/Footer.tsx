a8f51f88b8e746b49ddac18afbe4b7ca49776ce1
Magnus Palmblad <n.m.palmblad@lumc.nl>
2025-03-10 16:29:22 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <div>
       <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
         <div className="items-center grid-flow-col">
-          <p>Developed by <a href="http://www.https://www.esciencecenter.nl/" >Netherlands eSceince Center</a> in collaboration with <a href="https://www.lumc.nl/" >LUMC (Leids Universitair Medisch Centrum)</a> and <a href="https://www.uni-potsdam.de/en/university-of-potsdam">University of Potsdam</a>.
+          <p>Developed by <a href="http://www.https://www.esciencecenter.nl/" >Netherlands eScience Center</a> in collaboration with <a href="https://www.lumc.nl/" >LUMC (Leiden University Medical Center)</a> and <a href="https://www.uni-potsdam.de/en/university-of-potsdam">University of Potsdam</a>.
             Illustrations are designed by < a href="http://www.freepik.com" >Freepik</a >.</p>
         </div>
       </footer>
