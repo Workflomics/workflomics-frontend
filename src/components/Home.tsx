5edec82d9675cb6150265a50a83db1746d57e7b1
Aaron Strachardt <strachardt@proton.me>
2026-06-15 13:46:31 +0200

    chore: update alternatives home card copy, drop CWL-input download

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,7 +11,7 @@ const Home: FC = () => {
       <div className="flex space-x-7 p-11 justify-center">
         <HomeBox label="Generate and Benchmark workflows" descr="Explore bioinformatics workflows generated according to your description" imgUrl={generate_img} buttonText="Explore" isEnabled={true} component="/explore/domain" />
         <HomeBox label="Visualize benchmarks" descr="Upload and visualize the benchmarks performed locally" imgUrl={benchmark_img} buttonText="Benchmark" isEnabled={true} component="/benchmark/visualize" />
-        <HomeBox label="Generate Workflow Alternatives" descr="Upload a CWL workflow and explore alternative tool compositions using APE synthesis" imgUrl={benchmark_img} buttonText="Alternatives" isEnabled={true} component="/generate-alternatives" />
+        <HomeBox label="Generate Workflow Alternatives" descr="Upload an existing CWL workflow and explore alternative workflows" imgUrl={benchmark_img} buttonText="Alternatives" isEnabled={true} component="/generate-alternatives" />
         <HomeBox label="How to use Workflomics?" descr="Explore the Workflomics documentation" imgUrl={history_img} buttonText="User Guide" isEnabled={true} component="https://workflomics.readthedocs.io/en/latest/user-guide/web-interface.html" />
       </div>
     </div>
